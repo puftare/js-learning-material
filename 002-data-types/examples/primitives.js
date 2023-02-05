9b94a9fba7b2c2ce162fc61bca949721100d05aa
puftare <martin.potkonjak@hotmail.com>
2023-02-06 00:07:42 +0100

    added primitive data types

---
+++ 002-data-types/examples/primitives.js
@@ -0,0 +1,40 @@
+// Primitive Data Types
+// 1. Strings:
+// there are let and const variable naming as well, but it will be mentioned later
+
+var city = "Belgrade";
+var country;
+country = "Serbia";
+
+country = "India";
+
+var ime = "Jake";
+
+console.log("City: " + city + "Country: " + country + "Name: " + ime);
+
+// Numbers:
+// Numbers can be represented as integers: 1, 2, 3..., floats: 1.23, 1.24, 1.25..., binary: 001, 010, 011... etc.
+
+var age = 20;
+
+var price = 1.23;
+
+var binary = 001;
+
+var hexadecimal = 0x14f;
+
+console.log(age, price, binary, hexadecimal);
+
+// Booleans:
+// Booleans are like binary numbers, it can be either 0 or 1, in bollean meaning, eaither true or false.
+// Later on, we will mention truthy and Falsy values
+
+var happy = true;
+var sad = false;
+
+// Null and Undefined:
+// For now think of undefined as something you did not have a chance to name or declare its value
+// Where as null is something you defined to be of no value, therefore meaning, it has the value of nothing
+
+const empty = null;
+const empty2 = undefined;
