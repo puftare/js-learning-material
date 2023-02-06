fb4d2b7562d140c2369d8991c7b2b429f412ce21
puftare <martin.potkonjak@hotmail.com>
2023-02-06 18:44:17 +0100

    added mathematical operators

---
+++ 003-mathematical-operators/examples/math-operators.js
@@ -0,0 +1,23 @@
+// Addition
+var age = 10 + 23;
+console.log(age); // 33
+
+// Subtraction
+var points = 50 - 19;
+console.log(points);
+
+// Multiplication
+var score = 7 * 50;
+console.log(score);
+
+// Division
+var divide = 100 / 13;
+console.log(divide);
+
+// Modulus (Remainder)
+var mod = 9 % 3;
+console.log(mod);
+
+// Exponentiation
+var exp = 4 ** 3;
+console.log(exp);
---
+++ 003-mathematical-operators/examples/practise.js
@@ -0,0 +1,28 @@
+var points = 10;
+points++; // points = points + 1;
+console.log(points);
+
+var score = 20;
+score--;
+score--;
+console.log(score);
+
+score += 100;
+console.log(score);
+score -= 50;
+console.log(score);
+
+var age = 7;
+age *= 2; // age = age * 2;
+console.log(age);
+
+age /= 2;
+console.log(age);
+
+// age is 7 here
+// age %= 7; // age = age % 7;
+// console.log(age);
+
+// age is 7 here
+age **= 2;
+console.log(age);
