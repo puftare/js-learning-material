d1a815c7b5010639a07e28a3d7843c4904327cd0
puftare <martin.potkonjak@hotmail.com>
2023-02-07 03:46:41 +0100

    added boolean-operators

---
+++ 004-boolean-operators/exercise-solutions/problem-2.js
@@ -0,0 +1,25 @@
+/*
+    What would each of these variables evaluate to?
+
+    1. const one = true && "hi";
+    2. const two = false && "bye";
+    3. const three = true || "meow";
+    4. const four = false || "rawr";
+
+    Print them out to the console to see if you got them right.
+
+    HINT: 3 & 4 are more challenging, so don't worry if you don't
+    get them right away!
+*/
+
+var one = true && "hi";
+console.log(one);
+
+var two = false && "bye";
+console.log(two);
+
+var three = true || "meow";
+console.log(three);
+
+var four = false || "rawr";
+console.log(four);
