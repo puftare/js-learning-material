b9c162e616551237edb68adbac1cde06f9acb94e
puftare <martin.potkonjak@hotmail.com>
2023-02-09 12:18:56 +0100

    added while loops

---
+++ 007-while-loops/examples/while-loop.js
@@ -0,0 +1,16 @@
+var greeting = "hola!";
+
+// console.log(greeting);
+// console.log(greeting);
+// console.log(greeting);
+// console.log(greeting);
+// console.log(greeting);
+
+var number = 0;
+
+while (number < 3) {
+  console.log(greeting);
+  number++; // number = number + 1;
+}
+
+console.log("All done!");
