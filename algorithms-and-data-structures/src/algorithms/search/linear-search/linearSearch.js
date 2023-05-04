7a7e75068b19a2e770c90f043740ad142546a1ef
puftare <martin.potkonjak@hotmail.com>
2023-05-04 14:13:16 +0200

    added linear search algo

---
+++ algorithms-and-data-structures/src/algorithms/search/linear-search/linearSearch.js
@@ -0,0 +1,22 @@
+import Compare from '../../../utils/comparator/Compare';
+
+/**
+ * Linear search implementation.
+ *
+ * @param {*[]} array
+ * @param {*} seekElement
+ * @param {function(a, b)} [comparatorCallback]
+ * @return {number[]}
+ */
+export default function linearSearch(array, seekElement, comparatorCallback) {
+  const comparator = new Compare(comparatorCallback);
+  const foundIndices = [];
+
+  array.forEach((element, index) => {
+    if (comparator.equal(element, seekElement)) {
+      foundIndices.push(index);
+    }
+  });
+
+  return foundIndices;
+}
